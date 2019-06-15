a3be71ee9f74dea9514fbe19669462e801e35b31
malvarius <e.luismalvar@gmail.com>
2019-06-15 14:23:16 -0700

    made initial question

---
+++ assets/game.js
@@ -0,0 +1,19 @@
+// creating questions answer pairs
+
+const questions = [{
+    question: "What is the name of the Orcish Capital?",
+    correct: 'd',
+    answers: {
+        a: "Zandalar",
+        b: "Undercity",
+        c: "Stormwind",
+        d: "Orgrimmar"
+    }
+}
+
+
+]
+
+// displays question to screen
+let questDisp = function (num) { }
+$("#question").html("quest" + num +)
