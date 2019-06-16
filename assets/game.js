b29c2a5cc22f2686bff8fd9880e6120dee427c0f
Eric Malvar <47836797+malvarius@users.noreply.github.com>
2019-06-16 14:42:50 -0700

    added css stuff

--- assets/game.js
+++ assets/game.js
@@ -9,11 +9,46 @@ const questions = [{
         c: "Stormwind",
         d: "Orgrimmar"
     }
+},
+{
+    question: "What is the tallest Wow Race?",
+    correct: 'c',
+    answers: {
+        a: "Orc",
+        b: "Night Elf",
+        c: "Troll",
+        d: "Tauren"
+    }
+},
+{
+    question: "Who developed WoW?",
+    correct: 'a',
+    answers: {
+        a: "Blizzard",
+        b: "Activision",
+        c: "FIFA",
+        d: "Infinity Ward"
+    }
+},
+{
+    question: "What is the tallest Wow Race?",
+    correct: 'c',
+    answers: {
+        a: "Orc",
+        b: "Night Elf",
+        c: "Troll",
+        d: "Tauren"
+    }
 }
-
-
 ]
 
 // displays question to screen
-let questDisp = function (num) { }
-$("#question").html("quest" + num +)
+let questDisp = function () { 
+    for (i=0;i<questions.length;i++)
+$("#question").text(questions[0].question)
+$(".answer1").text(questions[0].answers.a)
+$(".answer2").text(questions[0].answers.b)
+$(".answer3").text(questions[0].answers.c)
+$(".answer4").text(questions[0].answers.d)
+}
+questDisp();
