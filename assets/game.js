69b91fee0d351ba0f7a8bda75528b0d8ccd85de6
Eric Malvar <47836797+malvarius@users.noreply.github.com>
2019-06-17 08:07:07 -0700

    added correct andswer function and timer

--- assets/game.js
+++ assets/game.js
@@ -2,7 +2,7 @@
 
 const questions = [{
     question: "What is the name of the Orcish Capital?",
-    correct: 'd',
+    correct: 'Orgrimmar',
     answers: {
         a: "Zandalar",
         b: "Undercity",
@@ -12,7 +12,7 @@ const questions = [{
 },
 {
     question: "What is the tallest Wow Race?",
-    correct: 'c',
+    correct: 'Troll',
     answers: {
         a: "Orc",
         b: "Night Elf",
@@ -22,7 +22,7 @@ const questions = [{
 },
 {
     question: "Who developed WoW?",
-    correct: 'a',
+    correct: 'Blizzard',
     answers: {
         a: "Blizzard",
         b: "Activision",
@@ -31,24 +31,43 @@ const questions = [{
     }
 },
 {
-    question: "What is the tallest Wow Race?",
-    correct: 'c',
+    question: "What is the druids owl form called?",
+    correct: 'Moonkin',
     answers: {
-        a: "Orc",
-        b: "Night Elf",
-        c: "Troll",
-        d: "Tauren"
+        a: "Owl Form",
+        b: "Moonkin",
+        c: "Evertree",
+        d: "Murloc"
     }
 }
 ]
+// Timer Function
+let timeStart = 30;
+let timer = function () {
+    timeStart--;
+    // Display time countdown to screen
+
+
+}
+window.setInterval(timer, 1000);
 
 // displays question to screen
-let questDisp = function () { 
-    for (i=0;i<questions.length;i++)
-$("#question").text(questions[0].question)
-$(".answer1").text(questions[0].answers.a)
-$(".answer2").text(questions[0].answers.b)
-$(".answer3").text(questions[0].answers.c)
-$(".answer4").text(questions[0].answers.d)
+let i = 0;
+let questDisp = function () {
+    $("#question").text(questions[i].question)
+    $(".answer1").text(questions[i].answers.a)
+    $(".answer2").text(questions[i].answers.b)
+    $(".answer3").text(questions[i].answers.c)
+    $(".answer4").text(questions[i].answers.d)
 }
+
+
 questDisp();
+// on click, checks if answer you clicked is correct or not
+$('.answers').on('click', function () {
+    if ($(this).text() === questions[0].correct) {
+        alert('correct!');
+    } else if ($(this).text() !== questions[0].correct) {
+        alert('Wrong!');
+    }
+})
